ce32cfe90a83cfd78f0103467cdc4608b4a1b8b4
Jiří Třečák <jiritrecak@gmail.com>
2015-11-20 20:36:32 +0100

    Added laurine install phase

--- laurine.rb
+++ laurine.rb
@@ -3,6 +3,7 @@ class Laurine < Formula
   url "https://github.com/JiriTrecak/Laurine/archive/0.1.0.tar.gz"
   sha1 "9d623fca0cdb78241348af789ba2fdc3653bc350"
 
-  def install
-  end
+def install
+    bin.install "LaurineGenerator.swift"
+end
 end
