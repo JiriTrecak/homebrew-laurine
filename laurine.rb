042d96e5b939d2c7478f28b3565bff4338392b30
Jiří Třečák <jiritrecak@gmail.com>
2015-11-20 20:16:43 +0100

    Added laurine 0.1.0 checksum

--- laurine.rb
+++ laurine.rb
@@ -1,7 +1,7 @@
 class Laurine < Formula
   homepage "https://github.com/JiriTrecak/Laurine/"
   url "https://github.com/JiriTrecak/Laurine/archive/0.1.0.tar.gz"
-  sha1 ""
+  sha1 "9d623fca0cdb78241348af789ba2fdc3653bc350"
 
   def install
   end
