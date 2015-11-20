042387a7da03bc879ecfbdab0a4c3a1a9434fcad
Jiří Třečák <jiritrecak@gmail.com>
2015-11-20 20:14:37 +0100

    Laurine Brew

---
+++ laurine.rb
@@ -0,0 +1,8 @@
+class Laurine < Formula
+  homepage "https://github.com/JiriTrecak/Laurine/"
+  url "https://github.com/JiriTrecak/Laurine/archive/0.1.0.tar.gz"
+  sha1 ""
+
+  def install
+  end
+end
