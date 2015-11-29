f29dca6fd09cef8038f8bf6d8cf2481c4b354f7e
Jiří Třečák <jiritrecak@gmail.com>
2015-11-29 17:13:22 +0100

    Brew Laurine 0.2.0

--- laurine.rb
+++ laurine.rb
@@ -1,7 +1,7 @@
 class Laurine < Formula
   homepage "https://github.com/JiriTrecak/Laurine/"
-  url "https://github.com/JiriTrecak/Laurine/archive/0.1.0.tar.gz"
-  sha1 "9d623fca0cdb78241348af789ba2fdc3653bc350"
+  url "https://github.com/JiriTrecak/Laurine/archive/0.2.0.tar.gz"
+  sha1 ""
 
 def install
     bin.install "LaurineGenerator.swift"
