8cfe7d4ff8ef423902f1ce2f84a29054050cb453
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-03-17 11:31:41 +0100

    Update to Laurine version 0.2.1

--- laurine.rb
+++ laurine.rb
@@ -1,9 +1,10 @@
 class Laurine < Formula
   homepage "https://github.com/JiriTrecak/Laurine/"
-  url "https://github.com/JiriTrecak/Laurine/archive/0.2.0.tar.gz"
-  sha1 "b77a4530ec85eac1ae4ea94de285071c1fb886c1"
+  url "https://github.com/JiriTrecak/Laurine/archive/0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "ad8e522b977379b067faa895434174056cb448480f23a6619b80933801c9ffe7"
 
-def install
-    bin.install "LaurineGenerator.swift"
-end
+  def install
+      bin.install "LaurineGenerator.swift"
+  end
 end
