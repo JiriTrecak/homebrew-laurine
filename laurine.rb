b9d85ae2b5868c6b50354bd7435b03e15ba8163e
Jiří Třečák <jiritrecak@gmail.com>
2016-03-24 11:02:54 +0100

    Laurine SHA1 fixed

--- laurine.rb
+++ laurine.rb
@@ -1,7 +1,7 @@
 class Laurine < Formula
   homepage "https://github.com/JiriTrecak/Laurine/"
   url "https://github.com/JiriTrecak/Laurine/archive/0.2.2.tar.gz"
-  sha1 "b77a4530ec85eac1ae4ea94de285071c1fb886c1"
+  sha1 "1a80770ee4a52ca830b97e934aa6a06aa6cf435b"
   version "0.2.2"
 
   def install
